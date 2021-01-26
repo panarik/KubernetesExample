46ca5553c150fb9d4fc88c0973287a6980abffda
panarik <43174022+panarik@users.noreply.github.com>
2021-01-26 17:42:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
 
-echo "<h1>Hello from Kubernetes</h1><br>"
+echo "<h1>Hello from Kubernetes</h1><br>";
 echo "Server IP Address is: $ip_server", "<br><p>";
 echo "Made by <font color=blue>panarik";
 
